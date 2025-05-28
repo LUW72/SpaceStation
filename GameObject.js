5b2108279bf359620dd3a6b3205e9d21329979f1
LUW72 <schneider.ivan337@gmail.com>
2025-05-28 22:09:52 +0200

    vending mach fix

--- GameObject.js
+++ GameObject.js
@@ -11,16 +11,18 @@ export const OBJECT_TYPES = {
   9: { name: "antenna", display_name: "Communication Antenna", interactable: false, objectId: 9 , texture: "./textures/obj_antenna.png", description: "Bee movie", solid: true},
 
 
-  10: { name: "door", display_name: "Back to Main Hall", interactable: true, objectId: 10 , texture: "./textures/door_01.png", description: "I will piss myself?", solid: true},
-  11: {name: "door", display_name:"Door to Storage Room", interactable: true, objectId: 11, texture:"./textures/door_01.png", description: "Bathroom?", solid: true},
-  12: {name: "door", display_name:"Door to Lab", interactable: true, objectId: 12, texture:"./textures/door_01.png", description: "Where is the pee room?", solid: true},
-
+  10: { name: "door", display_name: "Back to Main Hall", interactable: true, objectId: 10 , texture: "./textures/door_01.png", description: "I will piss myself?", solid: false},
+  11: {name: "door", display_name:"Door to Storage Room", interactable: true, objectId: 11, texture:"./textures/door_01.png", description: "Bathroom?", solid: false},
+  12: {name: "door", display_name:"Door to Lab", interactable: true, objectId: 12, texture:"./textures/door_01.png", description: "Where is the pee room?", solid: false},
 
 
   20: { name: "signpost", display_name: "Information Sign", interactable: true, objectId: 4, texture: "./textures/obj_sign.png", description: "Craftmine. ", text: "You could have it all...", solid: true},
   21: { name: "signpost", display_name: "Information Sign", interactable: true, objectId: 21, texture: "./textures/obj_sign.png", description: "Craftmine. ", text: "My empire of cheese...", solid: true},
   22: { name: "signpost", display_name: "Information Sign", interactable: true, objectId: 22, texture: "./textures/obj_sign.png", description: "Craftmine. ", text: "I will shit my pants...", solid: true},
   23: { name: "signpost", display_name: "Information Sign", interactable: true, objectId: 23, texture: "./textures/obj_sign.png", description: "Craftmine. ", text: "I will make you sneeeeeeze!!!", solid: true},
+
+  
+  24: { name:"floor_cable", display_name:"Broken Cables", interactable: false, objectId: 24, texture: "./textures/floor_01_cable.png", description: "Not the best way to handle a wire.", solid: false},
 
   99: { name: "empty", display_name: "", interactable: false, objectId: 99, texture: "./textures/obj_vendingMach.png", description: "", solid: true},  
 };
